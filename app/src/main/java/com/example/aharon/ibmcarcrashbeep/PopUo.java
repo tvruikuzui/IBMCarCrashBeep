7b55031237d762bd3bf8468d38f50dc636a3a066
Aharon <ahron901@gmail.com>
2017-09-12 15:44:13 +0300

    progress

--- app/src/main/java/com/example/aharon/ibmcarcrashbeep/PopUo.java
+++ app/src/main/java/com/example/aharon/ibmcarcrashbeep/PopUo.java
@@ -3,18 +3,24 @@ package com.example.aharon.ibmcarcrashbeep;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class PopUo extends AppCompatActivity {
+
+    private FrameLayout con;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_uo);
+
+        con = (FrameLayout) findViewById(R.id.con);
     }
 
     public void noOk(View view) {
-        Toast.makeText(this, " No ok", Toast.LENGTH_SHORT).show();
+        con.setVisibility(View.VISIBLE);
+
     }
 
     public void ok(View view) {
